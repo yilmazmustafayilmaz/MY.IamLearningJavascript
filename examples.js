51ed0df9f0154394197893eaa4333d9c9225e542
YILMAZ <mustafa.yilmaz@lbbilisim.com>
2025-09-23 10:04:25 +0300

    commit

--- examples.js
+++ examples.js
@@ -1,10 +1,11 @@
-const array = [ 
-    { name: "Jacop", age: 27 }, 
-    { name: "Lizzie", age: 33 }, 
-    { name: "Yosef", age: 29 },
-    { name: "Michael", age: 33 } ];
+const array = [
+  { name: "Jacop", age: 27 },
+  { name: "Lizzie", age: 33 },
+  { name: "Yosef", age: 29 },
+  { name: "Michael", age: 33 },
+];
 
-const filterArray = array.filter(x => x.age === 33);
+const filterArray = array.filter((x) => x.age === 33);
 console.log(filterArray);
 const stringifyArray = JSON.stringify(filterArray);
 console.log(stringifyArray);
